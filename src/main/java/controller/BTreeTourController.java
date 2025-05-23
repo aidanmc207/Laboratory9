838d9f9bfd56e691a28aee27ffae3cad0817c7aa
FortGamer207 <tatanmc2007@gmail.com>
2025-05-23 15:37:21 -0600

    .

--- src/main/java/controller/BTreeTourController.java
+++ src/main/java/controller/BTreeTourController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.scene.canvas.Canvas;
 
 public class BTreeTourController {
-
     private BTreeDrawer bTreeDrawer;
     private BTree bTree;
 
@@ -21,30 +20,28 @@ public class BTreeTourController {
 
     @javafx.fxml.FXML
     public void postOrderOnAction(ActionEvent actionEvent) {
-
+        bTreeDrawer.drawPostOrder(treeCanvas.getGraphicsContext2D(), bTree.getRoot());
     }
 
     @javafx.fxml.FXML
     public void inOrderOnAction(ActionEvent actionEvent) {
+        bTreeDrawer.drawInOrder(treeCanvas.getGraphicsContext2D(), bTree.getRoot());
+    }
+
+    @javafx.fxml.FXML
+    public void preOrderOnAction(ActionEvent actionEvent) {
+        bTreeDrawer.drawPreOrder(treeCanvas.getGraphicsContext2D(), bTree.getRoot());
     }
 
     @javafx.fxml.FXML
     public void randomizeOnAction(ActionEvent actionEvent) {
         int numNodes = 5 + util.Utility.random(20);
-
         bTree.clear();
 
-        //crear arbol con valores random
         for (int i = 0; i < numNodes; i++) {
-            // Generar un valor aleatorio entre 1 y 100
             Integer randomValue = util.Utility.random(100) + 1;
             bTree.add(randomValue);
         }
-
-        bTreeDrawer.draw(treeCanvas.getGraphicsContext2D(), bTree.getRoot());
-    }
-
-    @javafx.fxml.FXML
-    public void preOrderOnAction(ActionEvent actionEvent) {
+        bTreeDrawer.drawPreOrder(treeCanvas.getGraphicsContext2D(), bTree.getRoot());
     }
 }
