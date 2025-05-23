739eb5cd270b617bc58189976843bbefcfb77bea
chocol3it <murillosojoandree@gmail.com>
2025-05-22 22:16:30 -0600

    Config Controllers

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,14 +1,53 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import ucr.lab.HelloApplication;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Text txtMessage;
+
+    private void loadPage(String page) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(page));
+        try {
+            this.bp.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void Exit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void Home(ActionEvent event) {
+        this.txtMessage.setText("Laboratory No. 8");
+        this.bp.setCenter(ap);
+    }
+
+    @FXML
+    public void bTreeTourSortOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void graphicBTreeOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void bTreeOperationsOnAction(ActionEvent actionEvent) { loadPage("operations.fxml");
     }
 }
---
+++ src/main/java/controller/OperationsController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class OperationsController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void nodeHeightOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void randomizeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void containsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void treeHeightOnAction(ActionEvent actionEvent) {
+    }
+}
